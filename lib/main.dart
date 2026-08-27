9e70f1a9f0f3dfdc1f10f7a79fa3cdacd64c845a
muhaaqil6002@gmail.com <muhaaqil6002@gmail.com>
2026-08-27 18:01:05 +0700

    feat:create week 1 Flutter profile app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Profil Mahasiswa')),
+        body: const Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Icon(Icons.school, size: 72),
+              Text('Muhammad Aqil Azami', style: TextStyle(fontSize: 24)),
+              Text('Pemrograman Mobile - Minggu 1'),
+            ]),
+          ),
+        ),
+     );
+  }
+}
